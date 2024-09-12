a41b1253ccc57479aa895449824d78c6f2dc31be
dchair <a0913809823@gmail.com>
2024-09-12 10:16:32 +0800

    商品功能 - 實作查詢商品列表 (基礎實作)

--- src/main/java/com/chair/springboot_mall/dao/ProductDao.java
+++ src/main/java/com/chair/springboot_mall/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.chair.springboot_mall.dao;
 import com.chair.springboot_mall.dto.ProductRequest;
 import com.chair.springboot_mall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
--- src/main/java/com/chair/springboot_mall/service/ProductService.java
+++ src/main/java/com/chair/springboot_mall/service/ProductService.java
@@ -3,7 +3,12 @@ package com.chair.springboot_mall.service;
 import com.chair.springboot_mall.dto.ProductRequest;
 import com.chair.springboot_mall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
