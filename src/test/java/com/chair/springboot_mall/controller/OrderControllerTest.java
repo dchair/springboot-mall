b952a13250e5bbb19a029bbe093a65e99f0de6a1
dchair <a0913809823@gmail.com>
2024-11-05 13:40:33 +0800

    訂單功能 - 訂單測試(未完成)

---
+++ src/test/java/com/chair/springboot_mall/controller/OrderControllerTest.java
@@ -0,0 +1,188 @@
+package com.chair.springboot_mall.controller;
+
+import com.chair.springboot_mall.dto.BuyItem;
+import com.chair.springboot_mall.dto.CreateOrderRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class OrderControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private DefaultMockMvcBuilder mockMvcBuilder;
+
+    //創建訂單
+    @Test
+    @Transactional
+    public void createOrder_success() throws Exception {
+        CreateOrderRequest createOrderRequest = new CreateOrderRequest();
+        List<BuyItem> buyItemsList = new ArrayList<>();
+
+        BuyItem buyItem1 = new BuyItem();
+        buyItem1.setProductId(1);
+        buyItem1.setQuantity(5);
+        buyItemsList.add(buyItem1);
+
+        BuyItem buyItem2 = new BuyItem();
+        buyItem2.setProductId(2);
+        buyItem2.setQuantity(2);
+        buyItemsList.add(buyItem2);
+
+        createOrderRequest.setBuyItemList(buyItemsList);
+        String json = objectMapper.writeValueAsString(createOrderRequest);
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/users/{userId}/orders}",1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(201))
+                .andExpect(jsonPath("$.orderId",notNullValue()))
+                .andExpect(jsonPath("$.userId",equalTo(1)))
+                .andExpect(jsonPath("$.totalAmount",equalTo(750)))
+                .andExpect(jsonPath("$.orderItemList",notNullValue()))
+                .andExpect(jsonPath("$.createdDate",notNullValue()))
+                .andExpect(jsonPath("$.lastModifiedDate",notNullValue()));
+
+    }
+
+    @Test
+    @Transactional
+    public void createOrder_illegalArgument_emptyBuyItemList()throws Exception{
+        CreateOrderRequest createOrderRequest = new CreateOrderRequest();
+        List<BuyItem> buyItemList = new ArrayList<>();
+        createOrderRequest.setBuyItemList(buyItemList);
+
+        String json = objectMapper.writeValueAsString(createOrderRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/users/{userId}/orders",1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(400));
+    }
+
+    @Test
+    @Transactional
+    public void createOrder_userNotExist() throws Exception{
+        CreateOrderRequest createOrderRequest = new CreateOrderRequest();
+        List<BuyItem> buyItemList = new ArrayList<>();
+
+        BuyItem buyItem1 = new BuyItem();
+        buyItem1.setProductId(1);
+        buyItem1.setQuantity(1);
+        buyItemList.add(buyItem1);
+
+        createOrderRequest.setBuyItemList(buyItemList);
+
+        String json = objectMapper.writeValueAsString(createOrderRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/users/{userId}/orders",100)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(400));
+    }
+
+    @Test
+    @Transactional
+    public void createOrder_productNotExist() throws Exception{
+        CreateOrderRequest createOrderRequest = new CreateOrderRequest();
+        List<BuyItem> buyItemList = new ArrayList<>();
+        BuyItem buyItem1 = new BuyItem();
+        buyItem1.setProductId(100);
+        buyItem1.setQuantity(1);
+        buyItemList.add(buyItem1);
+
+        createOrderRequest.setBuyItemList(buyItemList);
+
+        String json = objectMapper.writeValueAsString(createOrderRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/users/{userId}/orders",1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(400));
+    }
+
+    @Test
+    @Transactional
+    public void createOrder_stockNotEnough() throws Exception{
+        CreateOrderRequest createOrderRequest = new CreateOrderRequest();
+        List<BuyItem> buyItemList = new ArrayList<>();
+
+        BuyItem buyItem1 = new BuyItem();
+        buyItem1.setProductId(1);
+        buyItem1.setQuantity(10000);
+        buyItemList.add(buyItem1);
+
+       createOrderRequest.setBuyItemList(buyItemList);
+
+       String json = objectMapper.writeValueAsString(createOrderRequest);
+
+       RequestBuilder requestBuilder = MockMvcRequestBuilders
+               .post("/users/{userId}/orders",1)
+               .contentType(MediaType.APPLICATION_JSON)
+               .content(json);
+
+       mockMvc.perform(requestBuilder)
+               .andExpect(status().is(400));
+    }
+
+    //查詢訂單列表
+    @Test
+    public void getOrder() throws Exception{
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/users/{userId}/orders",1);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.limit",notNullValue()))
+                .andExpect(jsonPath("$.offset",notNullValue()))
+                .andExpect(jsonPath("$.total",notNullValue()))
+                .andExpect(jsonPath("$.results",hasSize(2)))
+                .andExpect(jsonPath("$.results[0].orderId",notNullValue()))
+                .andExpect(jsonPath("$.results[0].userId",equalTo(1)))
+                .andExpect(jsonPath("$.results[0].totalAmount",equalTo(100000)))
+                .andExpect(jsonPath("$.results[0].orderItemList",hasSize(1)))
+                .andExpect(jsonPath("$.results[0].createdDate",notNullValue()))
+                .andExpect(jsonPath("$.results[0].lastModifiedDate",notNullValue()))
+                .andExpect(jsonPath("$.results[1].orderId",notNullValue()))
+                .andExpect(jsonPath("$.results[1].userId",equalTo(1)))
+                .andExpect(jsonPath("$.results[1].totalAmount",equalTo(500690)))
+                .andExpect(jsonPath("$.results[1].orderItemList",hasSize(3)))
+                .andExpect(jsonPath("$.results[1].createdDate",notNullValue()))
+                .andExpect(jsonPath("$.results[1].lastModifiedDate",notNullValue()));
+
+    }
+
+
+}
