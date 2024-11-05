eb3f05369c5d212742fa3b8102ab9bf23570f36c
dchair <a0913809823@gmail.com>
2024-11-05 17:49:47 +0800

    訂單功能 - 訂單測試功能

--- src/test/java/com/chair/springboot_mall/controller/UserControllerTest.java
+++ src/test/java/com/chair/springboot_mall/controller/UserControllerTest.java
@@ -13,7 +13,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
